e36dc5c7ea50a84b4dabf98755e3873ff01127e7
Shan Kim <shankim35@gmail.com>
2023-12-12 20:13:10 -0500

    added multiple page

--- myApp.js
+++ myApp.js
@@ -1,7 +1,19 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.write("hello");
+  const url = req.url
+  if(url == '/profile') {
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<p>this is the profile page</p>');
+    return res.end();
+  }
+  else if (url == '/') {
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<p>hello</p>');
+    return res.end();
+  }
+  res.setHeader('Content-Type', 'text/html')
+  res.write('<p>page not found</p>');
   res.end();
 })
 
