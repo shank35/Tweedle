4014421d9ced3d6b4371479eef6a88d906ae312b
Shan Kim <shankim35@gmail.com>
2023-12-12 20:35:47 -0500

    created post/get requests

--- routes.js
+++ routes.js
@@ -1,5 +1,7 @@
 const displayContent = (req, res) => {
   const url = req.url
+  const method = req.method;
+
   if(url == '/profile') {
     res.setHeader('Content-Type', 'text/html')
     res.write('<p>this is the profile page</p>');
@@ -10,6 +12,17 @@ const displayContent = (req, res) => {
     res.write('<p>hello</p>');
     return res.end();
   }
+  else if (url == '/settings' && method == "POST") {
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<h1>Form was submitted</h1>');
+    return res.end();
+  }
+  else if (url == '/settings') {
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<h1>setting page</h1><form action="/settings" method="POST"><input type="text"><button type="submit">press me</button></form>');
+    return res.end();
+  }
+
   res.setHeader('Content-Type', 'text/html')
   res.write('<p>page not found</p>');
   res.end();
